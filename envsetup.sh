5bfbd0bc23b06188a0c7e09f4f8d3174dd672a74
Prema Chand Alugu <premaca@gmail.com>
2016-09-01 00:21:49 +0530

    Java8 for Linux x86_64

--- envsetup.sh
+++ envsetup.sh
@@ -2003,7 +2003,7 @@ function set_java_home() {
               export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
               ;;
           *)
-              export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+              export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
               ;;
       esac
 
