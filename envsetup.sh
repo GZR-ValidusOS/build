a565efdda55ceba0be725033baf94c359586d9c0
Prema Chand Alugu <premaca@gmail.com>
2017-06-13 04:42:01 +0530

    envi: changelog generation added

--- envsetup.sh
+++ envsetup.sh
@@ -29,6 +29,7 @@ export HMM_DESCRIPTIVE=(
 "repopick: Utility to fetch changes from Gerrit."
 "installboot: Installs a boot.img to the connected device."
 "installrecovery: Installs a recovery.img to the connected device."
+"clog:   Generate the changelog, vendor/validus/tools/Changelog_Validus.txt"
 )
 
 function hmm() {
@@ -2109,3 +2110,25 @@ check_bash_version && {
 }
 
 export ANDROID_BUILD_TOP=$(gettop)
+
+function clog()
+{
+    txtrst=$(tput sgr0)             #  Reset
+    bldgrn=${txtbld}$(tput setaf 2) #  green
+
+    clog_cmd=vendor/validus/tools/changelog_validus
+    echo ""
+    echo ${bldgrn}"Executing $clog_cmd"${txtrst}
+
+    export Changelog=Changelog_Validus.txt
+
+    eval "$clog_cmd"
+    sed -i 's/project/   */g' $Changelog
+
+    cp $Changelog vendor/validus/
+    rm $Changelog
+
+    echo ${bldgrn}"Changelog generated at vendor/validus/$Changelog"${txtrst}
+
+    return;
+}
